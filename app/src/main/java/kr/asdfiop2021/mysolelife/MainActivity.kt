55a2d457844ceeb38111923f716a984e8f7f8fa3
semanticker <semanticker@gmail.com>
2021-10-24 23:00:01 +0900

    init

---
+++ app/src/main/java/kr/asdfiop2021/mysolelife/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.asdfiop2021.mysolelife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
