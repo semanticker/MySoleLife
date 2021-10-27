a15d29351d1a59f4e7094174bdc9c3db7e52dfb9
semanticker <semanticker@gmail.com>
2021-10-27 23:48:56 +0900

    회원가입 로그인 페이지 꾸미기 - 1

--- app/src/main/java/kr/asdfiop2021/mysolelife/auth/IntroActivity.kt
+++ app/src/main/java/kr/asdfiop2021/mysolelife/auth/IntroActivity.kt
@@ -1,12 +1,29 @@
 package kr.asdfiop2021.mysolelife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import kr.asdfiop2021.mysolelife.R
+import kr.asdfiop2021.mysolelife.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        binding.btnLogin.setOnClickListener {
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnJoin.setOnClickListener {
+            var intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/kr/asdfiop2021/mysolelife/auth/JoinActivity.kt
@@ -0,0 +1,12 @@
+package kr.asdfiop2021.mysolelife.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.asdfiop2021.mysolelife.R
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
