55745f28b90bc55065c82fa3775880a73184808e
semanticker <semanticker@gmail.com>
2021-12-17 21:20:11 +0900

    각각에 게시글에 대한 정보 불러오기   - 게시글의 아이디로 데이터 추출해서 로딩하는 방식

--- app/src/main/java/kr/asdfiop2021/mysolelife/board/BoardInsideActivity.kt
+++ app/src/main/java/kr/asdfiop2021/mysolelife/board/BoardInsideActivity.kt
@@ -3,9 +3,14 @@ package kr.asdfiop2021.mysolelife.board
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import kr.asdfiop2021.mysolelife.R
 import kr.asdfiop2021.mysolelife.databinding.ActivityBoardInsideBinding
+import kr.asdfiop2021.mysolelife.utils.FBRef
 
 class BoardInsideActivity : AppCompatActivity() {
 
@@ -19,6 +24,8 @@ class BoardInsideActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_board_inside)
 
+        /*
+        첫번째 방법
         val title = intent.getStringExtra("title").toString()
         val content = intent.getStringExtra("content").toString()
         val time = intent.getStringExtra("time").toString()
@@ -26,6 +33,33 @@ class BoardInsideActivity : AppCompatActivity() {
         binding.textTitle.text = title
         binding.textTime.text = time
         binding.textContent.text = content
+        */
 
+        // 두번째 방법
+        val key = intent.getStringExtra("key")
+        getBoardData(key.toString())
+    }
+
+    private fun getBoardData(key: String) {
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                Log.d(TAG, dataSnapshot.toString())
+                val dataModel = dataSnapshot.getValue(BoardModel::class.java)
+                Log.d(TAG, dataModel!!.title)
+
+                binding.textTitle.text = dataModel!!.title
+                binding.textContent.text = dataModel!!.content
+                binding.textTime.text = dataModel!!.time
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+
+        FBRef.boardRef.child(key).addValueEventListener(postListener)
     }
 }
--- app/src/main/java/kr/asdfiop2021/mysolelife/fragments/TalkFragment.kt
+++ app/src/main/java/kr/asdfiop2021/mysolelife/fragments/TalkFragment.kt
@@ -32,6 +32,7 @@ class TalkFragment : Fragment() {
     private lateinit var binding : FragmentTalkBinding
 
     private val boardDataList = mutableListOf<BoardModel>()
+    private val boardKeyList = mutableListOf<String>()
 
     private lateinit var boardRVAdater : BoardListVAdapter
 
@@ -52,16 +53,19 @@ class TalkFragment : Fragment() {
 
         binding.listViewBoard.setOnItemClickListener { adapterView, view, position, id ->
 
-            val intent = Intent(context, BoardInsideActivity::class.java)
+            // 첫번째 방법으로 listview에 있는 title content time 다 다른 액티비티로 전달해줘서 만들기
+            /*val intent = Intent(context, BoardInsideActivity::class.java)
             intent.putExtra("title", boardDataList[position].title)
             intent.putExtra("content", boardDataList[position].content)
             intent.putExtra("time", boardDataList[position].time)
+            startActivity(intent)*/
+
+            // 두번째 방법으로는 Firebase에 있는 board에 대한 데이터의 id를 기반으로 다시 데이터를 받아오는 방법
+            val intent = Intent(context, BoardInsideActivity::class.java)
+            intent.putExtra("key", boardKeyList[position])
             startActivity(intent)
-
         }
-        // 첫번째 방법으로 listview에 있는 title content time 다 다른 액티비티로 전달해줘서 만들기
 
-        // 두번째 방법으로는 Firebase에 있는 board에 대한 데이터의 id를 기반으로 다시 데이터를 받아오는 방법
 
 
 
@@ -103,9 +107,11 @@ class TalkFragment : Fragment() {
                     Log.d("ContentListActivity", dataModel.key.toString())
                     val item = dataModel.getValue(BoardModel::class.java)
                     boardDataList.add(item!!)
+                    boardKeyList.add(dataModel.key.toString())
                 }
 
-                boardDataList.reverse();
+                boardKeyList.reverse()
+                boardDataList.reverse()
 
                 boardRVAdater.notifyDataSetChanged()
 
