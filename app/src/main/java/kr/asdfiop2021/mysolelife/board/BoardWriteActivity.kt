3211e1a9834185ac0d72c1f93093396a99dff864
semanticker <semanticker@gmail.com>
2021-12-19 22:55:47 +0900

    게시글의 이미지를 파이어베이스에 저장하기

--- app/src/main/java/kr/asdfiop2021/mysolelife/board/BoardWriteActivity.kt
+++ app/src/main/java/kr/asdfiop2021/mysolelife/board/BoardWriteActivity.kt
@@ -1,17 +1,22 @@
 package kr.asdfiop2021.mysolelife.board
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import kr.asdfiop2021.mysolelife.R
 import kr.asdfiop2021.mysolelife.contentsList.BookmarkModel
 import kr.asdfiop2021.mysolelife.databinding.ActivityBoardWriteBinding
 import kr.asdfiop2021.mysolelife.utils.FBAuth
 import kr.asdfiop2021.mysolelife.utils.FBRef
+import java.io.ByteArrayOutputStream
 
 class BoardWriteActivity : AppCompatActivity() {
 
@@ -36,6 +41,8 @@ class BoardWriteActivity : AppCompatActivity() {
             Log.d(TAG, uid);
             Log.d(TAG, time);
 
+            val key = FBRef.boardRef.push().key.toString()
+
             // 북마크에 등록이 되어 있지 않은 경우
             /*
                 board
@@ -43,10 +50,13 @@ class BoardWriteActivity : AppCompatActivity() {
                         boardModel(title, content, uid, time)
              */
             FBRef.boardRef
-                .push()
+                //.push()
+                .child(key)
                 .setValue(BoardModel(title, content, uid, time))
 
             Toast.makeText(this, "게시글 입력 완료", Toast.LENGTH_LONG).show()
+
+            imageUpload(key)
 
             finish()
         }
@@ -54,6 +64,33 @@ class BoardWriteActivity : AppCompatActivity() {
         binding.imgPlus.setOnClickListener {
             val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
             startActivityForResult(gallery, 100)
+        }
+    }
+
+    private fun imageUpload(key : String){
+
+        val storage = Firebase.storage
+
+        val storageRef = storage.reference
+        //val mountainsRef = storageRef.child("mountains.jpg")
+        val mountainsRef = storageRef.child(key + ".png")
+
+        val imageView = binding.imgPlus
+
+        // Get the data from an ImageView as bytes
+        imageView.isDrawingCacheEnabled = true
+        imageView.buildDrawingCache()
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = mountainsRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
         }
     }
 
