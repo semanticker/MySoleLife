76472cff4c45a27a07b0ecbb74d74dad1ba005d4
semanticker <semanticker@gmail.com>
2021-12-20 21:22:02 +0900

    게시글 이미지 다운로드   - Glide 종속성 추가

--- app/src/main/java/kr/asdfiop2021/mysolelife/board/BoardInsideActivity.kt
+++ app/src/main/java/kr/asdfiop2021/mysolelife/board/BoardInsideActivity.kt
@@ -3,11 +3,15 @@ package kr.asdfiop2021.mysolelife.board
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import kr.asdfiop2021.mysolelife.R
 import kr.asdfiop2021.mysolelife.databinding.ActivityBoardInsideBinding
 import kr.asdfiop2021.mysolelife.utils.FBRef
@@ -38,6 +42,25 @@ class BoardInsideActivity : AppCompatActivity() {
         // 두번째 방법
         val key = intent.getStringExtra("key")
         getBoardData(key.toString())
+
+        getImageData(key.toString())
+
+
+    }
+
+    private fun getImageData(key: String) {
+
+        // Reference to an image file in Cloud Storage
+        val storageReference = Firebase.storage.reference.child(key + ".png")
+
+        // ImageView in your Activity
+        val imageView = findViewById<ImageView>(R.id.imageArea)
+
+        // Download directly from StorageReference using Glide
+        // (See MyAppGlideModule for Loader registration)
+        Glide.with(this /* context */)
+            .load(storageReference)
+            .into(imageView)
     }
 
     private fun getBoardData(key: String) {
