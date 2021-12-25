8b7aa6083f9be91f16143d493f2baf4971e930d4
semanticker <semanticker@gmail.com>
2021-12-25 21:46:59 +0900

    로그아웃 만들기

--- app/src/main/java/kr/asdfiop2021/mysolelife/setting/SettingActivity.kt
+++ app/src/main/java/kr/asdfiop2021/mysolelife/setting/SettingActivity.kt
@@ -1,40 +1,39 @@
-package kr.asdfiop2021.mysolelife
+package kr.asdfiop2021.mysolelife.setting
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kr.asdfiop2021.mysolelife.R
 import kr.asdfiop2021.mysolelife.auth.IntroActivity
-import kr.asdfiop2021.mysolelife.setting.SettingActivity
 
-class MainActivity : AppCompatActivity() {
+class SettingActivity : AppCompatActivity() {
 
     private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        auth = Firebase.auth
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_setting)
 
-        findViewById<ImageView>(R.id.btnSetting).setOnClickListener {
-            val intent = Intent(this, SettingActivity::class.java)
-            startActivity(intent)
-        }
+        auth = Firebase.auth
 
-/*
-        findViewById<Button>(R.id.btnLogout).setOnClickListener {
+        val btnLogout = findViewById<Button>(R.id.btnLogout)
+        val logoutButton : Button = findViewById(R.id.btnLogout)
+
+        logoutButton.setOnClickListener {
 
             auth.signOut()
+
+            Toast.makeText(this, "로그아웃", Toast.LENGTH_LONG).show()
 
             val intent = Intent(this, IntroActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(intent)
         }
-*/
 
     }
 }
