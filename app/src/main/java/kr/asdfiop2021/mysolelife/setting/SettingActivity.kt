e0faa44bffcb371bbebd753cfcb74f091e206d4b
semanticker <semanticker@gmail.com>
2021-12-29 23:15:00 +0900

    로그아웃 버그 수정

--- app/src/main/java/kr/asdfiop2021/mysolelife/setting/SettingActivity.kt
+++ app/src/main/java/kr/asdfiop2021/mysolelife/setting/SettingActivity.kt
@@ -31,7 +31,7 @@ class SettingActivity : AppCompatActivity() {
             Toast.makeText(this, "로그아웃", Toast.LENGTH_LONG).show()
 
             val intent = Intent(this, IntroActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
 
